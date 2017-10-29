3c6c7a79ebedf7b8393f0d7cdc0e66c39d742db3
Aslan1st <aslan1st@mac.com>
2017-10-29 00:09:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 def display_board
    board_game_row = Array.new(9, "   ")
    board_game_devision = "-----------"
-   print board_game_row[0] + | + board_game_row[1]
+   print board_game_row[0] + "|" + board_game_row[1] + "|" + board_game_row[2]
 end
