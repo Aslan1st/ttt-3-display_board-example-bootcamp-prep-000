08a0347d107a6548503997aae14b4d72504d9115
Aslan1st <aslan1st@mac.com>
2017-10-29 00:14:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,9 @@
 def display_board
    board_game_row = Array.new(9, "   ")
    board_game_devision = "-----------"
-   puts board_game_row[0] + "|" + board_game_row[1] + "|" + board_game_row[2], board_game_devision
+   puts board_game_row[0] + "|" + board_game_row[1] + "|" + board_game_row[2],
+        board_game_devision,
+        board_game_row[3] + "|" + board_game_row[4] + "|" + board_game_row[5],
+        board_game_devision,
+        board_game_row[6] + "|" + board_game_row[7] + "|" + board_game_row[8]
 end
